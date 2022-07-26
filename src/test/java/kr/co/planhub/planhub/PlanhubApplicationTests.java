744f32f46b58eb2d7fa3b209f4b8921ba802405c
yu <bounggun.yu@gmailc.om>
2022-07-26 21:41:39 +0900

    planhub 초기 설정

---
+++ src/test/java/kr/co/planhub/planhub/PlanhubApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.planhub.planhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
