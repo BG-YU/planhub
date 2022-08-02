146d97b054813b08ab39675c41784836e56103fc
yu <bounggun.yu@gmailc.om>
2022-08-02 23:29:07 +0900

    pagingTasks 메소드 리팩토링

--- src/main/java/kr/co/planhub/services/task/TaskServiceImpl.java
+++ src/main/java/kr/co/planhub/services/task/TaskServiceImpl.java
@@ -1,9 +1,6 @@
 package kr.co.planhub.services.task;
 
-import kr.co.planhub.domain.task.SubTask;
-import kr.co.planhub.domain.task.SubTaskRepository;
-import kr.co.planhub.domain.task.Task;
-import kr.co.planhub.domain.task.TaskRepository;
+import kr.co.planhub.domain.task.*;
 import kr.co.planhub.response.task.TaskResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +8,10 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 
@@ -25,29 +24,55 @@ public class TaskServiceImpl implements TaskService{
     private final SubTaskRepository subTaskRepository;
 
     @Override
-    public List<TaskResponse> findTask(Long userId, Long id) {
-        List<Task> tasks = taskRepository.findAllByUserIdAndIdLessThanOrderByIdDesc(userId, id);
-        List<SubTask> subTasks = subTaskRepository.findAllByTaskIn(tasks);
-        return subTasks.stream()
+    public List<TaskResponse> pagingTasks(Long userId, Long taskId, Long pageSize) {
+        return findSubTasksByTasks(findTasksBySearchCondition(userId, taskId, pageSize)).stream()
                 .collect(groupingBy(SubTask::getTask))
                 .entrySet()
                 .stream()
-                .map(task -> TaskResponse.of(
-                        task.getKey().getId(),
-                        task.getKey().getUserId(),
-                        task.getKey().getUpperId(),
-                        task.getKey().getTitle(),
-                        task.getKey().getReadCount(),
-                        task.getValue()
-                                .stream()
-                                .map(subTask -> TaskResponse.ItemResponse.of(
-                                        subTask.getId(),
-                                        subTask.getItem(),
-                                        subTask.getCheckType(),
-                                        subTask.getSort()))
-                                .collect(toList())
-                ))
+                .map(TaskServiceImpl::convertToTaskResponse)
+                .sorted(comparing(TaskResponse::getId).reversed())
                 .collect(toList());
+    }
+
+    private static TaskResponse convertToTaskResponse(Map.Entry<Task, List<SubTask>> task) {
+        return TaskResponse.of(
+                task.getKey().getId(),
+                task.getKey().getUserId(),
+                task.getKey().getUpperId(),
+                task.getKey().getTitle(),
+                task.getKey().getReadCount(),
+                convertToItemResponseList(task.getValue())
+        );
+    }
+
+    private static List<TaskResponse.ItemResponse> convertToItemResponseList(List<SubTask> subTasks) {
+        return subTasks.stream()
+                .map(TaskServiceImpl::convertToItemResponse)
+                .collect(toList());
+    }
+
+    private static TaskResponse.ItemResponse convertToItemResponse(SubTask subTask) {
+        return TaskResponse.ItemResponse.of(
+                subTask.getId(),
+                subTask.getItem(),
+                subTask.getCheckType(),
+                subTask.getSort());
+    }
+
+    private List<Task> findTasksBySearchCondition(Long userId, Long taskId, Long pageSize) {
+        return findAllTasks(pagingCondition(userId, taskId, pageSize));
+    }
+
+    private List<Task> findAllTasks(TaskSearchCondition condition) {
+        return taskRepository.findAll(condition);
+    }
+
+    private TaskSearchCondition pagingCondition(Long userId, Long taskId, Long pageSize) {
+        return TaskSearchCondition.pagingCondition(userId, taskId, pageSize);
+    }
+
+    private List<SubTask> findSubTasksByTasks(List<Task> tasks) {
+        return subTaskRepository.findAllByTaskIn(tasks);
     }
 
     @Override
@@ -69,7 +94,8 @@ public class TaskServiceImpl implements TaskService{
     }
 
     private List<SubTask> createItems(Task task, List<String> items) {
-        AtomicInteger sort = new AtomicInteger(1);
+        final int INIT_ATOMIC_VALUE = 1;
+        AtomicInteger sort = new AtomicInteger(INIT_ATOMIC_VALUE);
         return items.stream()
                 .map(item -> SubTask.create(task, item, sort.getAndIncrement()))
                 .collect(toList());
